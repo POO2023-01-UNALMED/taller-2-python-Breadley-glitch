615117f6521f012a66eed18f98b000dff6ba9a7d
Breadley-glitch <124929511+Breadley-glitch@users.noreply.github.com>
2023-02-20 23:12:43 -0500

    intento1

---
+++ main.py
@@ -0,0 +1,58 @@
+class Auto:
+
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio =precio
+        self.asientos =asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        numeroAsientos = 0
+
+        for asiento in self.asientos:
+            if isinintance(asiento, Asiento):
+                numeroAsientos +=1
+
+        return numeroAsientos
+
+    def verficarIntegridad(self):
+        for asiento in self.asientos:
+            if asiento != NONE:
+                if asiento.registro != self.registro:
+                    return "Las piezas no son originales"
+
+        if self.motor.registro != self.registro:
+            return "Las piezas no son originales"
+
+        return "Auto original"
+
+class Asiento:
+
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        colors = ["rojo", "amarillo", "negro", "blanco"]
+        if color in colors:
+            self.color = color
+
+class Motor:
+
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro =registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self,tipo):
+        types = ["electrico", "gasolina"]
+        if tipo in types:
+            self.tipo = tipo
